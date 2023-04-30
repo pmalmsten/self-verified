67c824b934c5b047e5349a04094ce47fbccf96e9
Paul Malmsten <pmalmsten@gmail.com>
2023-04-29 22:23:30 -0700

    Popup UI now checks whether current page is a social link from single trusted page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Browser from 'webextension-polyfill';
+import * as cheerio from 'cheerio';
+import { SocialLinks } from 'social-links'; 
+import { tab } from '@testing-library/user-event/dist/tab';
+
+const socialLinks = new SocialLinks();
+const trustedUrl = "https://www.npr.org/sections/money/"
 
 function App() {
+  const [verified, setVerified] = useState<boolean | undefined>(undefined);
+
+ fetch(new URL(trustedUrl))
+  .then(response => response.text())
+  .then(bodyText => {
+    let $ = cheerio.load(bodyText);
+    let aTags = $("a").get();
+
+    let linkUrls = aTags
+      .map(a => a.attribs.href)
+      .filter(url => url !== undefined && url.length > 0);
+
+    let uniqueLinkUrls = Array.from(new Set(linkUrls));
+
+    console.log("Links: " + uniqueLinkUrls);
+
+    let filteredToSocialLinks = uniqueLinkUrls.filter(url => socialLinks.detectProfile(url).length > 0);
+    console.log("Social Links: " + filteredToSocialLinks);
+
+    // If current browser tab URL matches a filtered social link, then set the
+    // state of verified to true. Otherwise, set it to false.
+    return Browser.tabs.query({active: true, currentWindow: true})
+      .then(tabs => {
+          let currentTab = tabs[0];
+          let currentTabUrl = currentTab.url;
+          console.log("Current Tab URL: " + currentTabUrl);
+
+          if (currentTabUrl === undefined) {
+            throw new Error("Current tab URL is undefined");
+          }
+
+          setVerified(filteredToSocialLinks.includes(currentTabUrl));
+        })
+  })
+  .catch(err => console.error(err));
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {verified === undefined ? 'Loading...' : verified ? 'Verified' : 'Not Verified'}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
