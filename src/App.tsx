f24576a4b6a6f34951ddf0e7e130745e6d4c90c9
Paul Malmsten <pmalmsten@gmail.com>
2023-04-30 11:51:27 -0700

    Play with loading trusted URLs in the background and using storage

--- src/App.tsx
+++ src/App.tsx
@@ -3,48 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 import Browser from 'webextension-polyfill';
 import * as cheerio from 'cheerio';
-import { SocialLinks } from 'social-links'; 
-import { tab } from '@testing-library/user-event/dist/tab';
+import { SocialLinks } from 'social-links';
 
 const socialLinks = new SocialLinks();
 const trustedUrl = "https://www.npr.org/sections/money/"
 
+interface Storage {
+  // trustedSocialUrls object, where keys are social URLs
+  trustedSocialUrls?: { [key: string]: {} };
+}
+
+let currentStorage: Storage = {};
+const storageInitialized = Browser.storage.local.get().then(storage => {
+  currentStorage = storage as Storage;
+
+  console.log("Storage initialized: " + JSON.stringify(currentStorage));
+});
+
 function App() {
   const [verified, setVerified] = useState<boolean | undefined>(undefined);
 
- fetch(new URL(trustedUrl))
-  .then(response => response.text())
-  .then(bodyText => {
-    let $ = cheerio.load(bodyText);
-    let aTags = $("a").get();
+  storageInitialized.then(async () => {
+    // Check if the active tab's url is in the trustedSocialUrls object
+    const tabs = await Browser.tabs.query({ active: true, currentWindow: true });
+    let currentTab = tabs[0];
+    let currentTabUrl = currentTab.url;
 
-    let linkUrls = aTags
-      .map(a => a.attribs.href)
-      .filter(url => url !== undefined && url.length > 0);
+    console.log("Current Tab URL: " + currentTabUrl);
 
-    let uniqueLinkUrls = Array.from(new Set(linkUrls));
+    if (currentTabUrl === undefined) {
+      throw new Error("Current tab URL is undefined");
+    }
 
-    console.log("Links: " + uniqueLinkUrls);
-
-    let filteredToSocialLinks = uniqueLinkUrls.filter(url => socialLinks.detectProfile(url).length > 0);
-    console.log("Social Links: " + filteredToSocialLinks);
-
-    // If current browser tab URL matches a filtered social link, then set the
-    // state of verified to true. Otherwise, set it to false.
-    return Browser.tabs.query({active: true, currentWindow: true})
-      .then(tabs => {
-          let currentTab = tabs[0];
-          let currentTabUrl = currentTab.url;
-          console.log("Current Tab URL: " + currentTabUrl);
-
-          if (currentTabUrl === undefined) {
-            throw new Error("Current tab URL is undefined");
-          }
-
-          setVerified(filteredToSocialLinks.includes(currentTabUrl));
-        })
-  })
-  .catch(err => console.error(err));
+    setVerified(currentStorage.trustedSocialUrls?.hasOwnProperty(currentTabUrl));
+  });
 
   return (
     <div className="App">
--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,54 @@
-export {}
+import Browser from 'webextension-polyfill';
+import * as cheerio from 'cheerio';
+import { SocialLinks } from 'social-links';
+
+const socialLinks = new SocialLinks();
+const trustedUrls = ["https://www.npr.org/sections/money/"]
+
+interface Storage {
+    // trustedSocialUrls object, where keys are social URLs
+    trustedSocialUrls?: { [key: string]: {} };
+}
+
+let currentStorage: Storage = {};
+const storageInitialized = Browser.storage.local.get().then(storage => {
+    currentStorage = storage as Storage;
+
+    console.log("Storage initialized: " + JSON.stringify(currentStorage));
+});
+
+let promises = trustedUrls.map(async trustedUrl => {
+    let bodyText = await fetch(new URL(trustedUrl))
+        .then(response => response.text());
+
+    let $ = cheerio.load(bodyText);
+    let aTags = $("a").get();
+
+    // Get all the links on the page
+    let linkUrls = aTags
+        .map(a => a.attribs.href)
+        .filter(url => url !== undefined && url.length > 0);
+
+    let uniqueLinkUrls = Array.from(new Set(linkUrls));
+
+    console.log("Links: " + uniqueLinkUrls);
+
+    // Filter the links to only social links
+    let filteredToSocialLinks = uniqueLinkUrls.filter(url => socialLinks.detectProfile(url).length > 0);
+    console.log("Social Links: " + filteredToSocialLinks);
+
+    // Add all the social links to the trusted set
+    await storageInitialized;
+    filteredToSocialLinks.forEach(url => {
+        if (currentStorage.trustedSocialUrls === undefined) {
+            currentStorage.trustedSocialUrls = {};
+        }
+
+        currentStorage.trustedSocialUrls[url] = {};
+    });
+});
+
+// Wait for all the promises to finish
+Promise.all(promises)
+    .then(() => Browser.storage.local.set(currentStorage))
+    .then(() => console.log("Storage set"));
